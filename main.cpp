ad8a9ef9a9d898539366ac5083a25029b203a596
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-20 23:09:38 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file main.cpp
+ * @author Thavarshan Thayananthajothy (tjthavarshan@gmail.com) <CL/HDCSE/95/15>
+ * @brief Rathnayaka Gym Invoice Generator.
+ * @version 1.0
+ * @date 2021-02-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <cstdio>
+#include <string>
+
+/**
+ * @brief Execute the program.
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
