3a69400f082e559486adcab14dc6c3392243eeac
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-02-28 23:59:28 +0530

    Update cmake config

--- src/includes/utils.h
+++ src/includes/utils.h
@@ -4,6 +4,7 @@
 
 std::string askName();
 const bool inRange(unsigned low, unsigned high, unsigned input);
+std::string currentDateTime();
 const bool fileExists(std::string name);
 void writeFile(std::string file, std::string content);
 void readFile(std::string file);
--- tests/util_test.cpp
+++ tests/util_test.cpp
@@ -6,3 +6,11 @@ TEST(util_test, inRange)
     EXPECT_TRUE(inRange(45, 50, 47));
     EXPECT_FALSE(inRange(45, 50, 30));
 }
+
+TEST(invoice_tests, currentDateTime)
+{
+    time_t now = time(0);
+    std::string dateTime = ctime(&now);
+
+    EXPECT_EQ(dateTime, currentDateTime());
+}
